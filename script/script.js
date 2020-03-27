7b59a36a1a84a34a1ea4a173e1efa895fba599e6
Andrei Shakh <andreishakhs@gmail.com>
2020-03-27 11:11:03 +0300

    lesson02

--- script/script.js
+++ script/script.js
@@ -9,3 +9,19 @@ alert ('First lesson');
 
 console.log ('First lesson');
 
+money = 9000;
+income = 'игры';
+addExpenses = 'интернет, комуналка, транспорт';
+deposit = true;
+mission = 900000;
+period = 12;
+
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(typeof money);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев', 'Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
